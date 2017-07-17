51982d8dcebf849004270d8999ce6d71c8bb151f
Grematorium-PC\Grematorium <bagas.dewangkara@gmail.com>
2017-07-17 20:19:10 +0700

    bug fix

--- common/models/ProjectSearch.php
+++ common/models/ProjectSearch.php
@@ -19,7 +19,7 @@ use common\models\Project;
     {
         return [
             [['id', 'customer_id', 'city_id', 'estimated_budget', 'fixed_budget', 'status'], 'integer'],
-            [['name', 'type', 'description', 'address', 'start', 'finish', 'created_by', 'created_at', 'updated_at', 'updated_by'], 'safe'],
+            [['name', 'type', 'description', 'address', 'start', 'finish'], 'safe'],
         ];
     }
 
@@ -64,17 +64,14 @@ use common\models\Project;
             'start' => $this->start,
             'finish' => $this->finish,
             'status' => $this->status,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+           
         ]);
 
         $query->andFilterWhere(['like', 'name', $this->name])
             ->andFilterWhere(['like', 'type', $this->type])
             ->andFilterWhere(['like', 'description', $this->description])
-            ->andFilterWhere(['like', 'address', $this->address])
-            ->andFilterWhere(['like', 'created_by', $this->created_by])
-            ->andFilterWhere(['like', 'updated_by', $this->updated_by]);
-
+            ->andFilterWhere(['like', 'address', $this->address]);
+           
         return $dataProvider;
     }
      public function search_worker($params)
@@ -102,17 +99,14 @@ use common\models\Project;
             'start' => $this->start,
             'finish' => $this->finish,
             'status' => 0,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-
+          
         ]);
 
         $query->andFilterWhere(['like', 'name', $this->name])
             ->andFilterWhere(['like', 'type', $this->type])
             ->andFilterWhere(['like', 'description', $this->description])
             ->andFilterWhere(['like', 'address', $this->address])
-            ->andFilterWhere(['like', 'created_by', $this->created_by])
-            ->andFilterWhere(['like', 'updated_by', $this->updated_by]);
+            ;
              // ->andFilterWhere(['like', 0, $this->status]);
         return $dataProvider;
     }
